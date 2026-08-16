9ae251652de23e51dfe9c68c294aed7ee49f99fa
mykola <boberanonim5@gmail.com>
2026-08-16 22:35:46 +0300

    Added Player

---
+++ Player.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Player.h"
+
+Player::Player() {
+    hp = 100;
+    attack = 10;
+}
+
+void Player::attackEnemy() {
+    // We'll implement this later
+}
+
+void Player::takeDamage(int damage) {
+    hp -= damage;
+
+    if (hp <= 0) {
+        hp = 0;
+        std::cout << "You died\n";
+    } else {
+        std::cout << "Your HP: " << hp << '\n';
+    }
+}
+
+int Player::getHp() {
+    return hp;
+}
+
+int Player::getAttack() {
+    return attack;
+}
---
+++ Player.h
@@ -0,0 +1,19 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+class Player {
+private:
+    int hp;
+    int attack;
+
+public:
+    Player();
+
+    void attackEnemy();
+    void takeDamage(int damage);
+
+    int getHp();
+    int getAttack();
+};
+
+#endif
